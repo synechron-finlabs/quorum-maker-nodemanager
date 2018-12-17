1ccf231603acf7ea89e1e972f0b4c8301c1d8436
Dhyan Raj <dhyan.raj@synechron.com>
2018-12-17 03:16:06 -0500

    Updated as per Solidity V 0.5

--- NetworkManagerContract.sol
+++ NetworkManagerContract.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.4.23;
+pragma solidity^0.5.0;
 contract NetworkManagerContract {
 
     uint nodeCounter;
@@ -17,7 +17,7 @@ contract NetworkManagerContract {
     
     event print(string nodeName, string role,string publickey, string enode, string ip, string id);
 
-    function registerNode(string n, string r, string p,string e, string ip, string id) public {
+    function registerNode(string memory n, string memory r, string memory p, string memory e, string memory ip, string memory id) public {
 
         nodes[e].publickey = p;
         nodes[e].nodeName = n;
@@ -29,7 +29,7 @@ contract NetworkManagerContract {
     
     }
    
-    function getNodeDetails(uint16 _index) constant public returns (string n, string r,string p,string ip,string e,string id,uint i) {
+    function getNodeDetails(uint16 _index) public view returns (string memory n, string memory r, string memory p, string memory ip, string memory e, string memory id, uint i) {
         NodeDetails memory nodeInfo = nodes[enodeList[_index]];
         return (
             nodeInfo.nodeName,
@@ -42,11 +42,11 @@ contract NetworkManagerContract {
         );
     }
 
-    function getNodesCounter() public constant  returns (uint) {
+    function getNodesCounter() public view  returns (uint) {
         return enodeList.length;
     }
 
-    function updateNode(string n, string r, string p, string e, string ip, string id) public {
+    function updateNode(string memory n, string memory r, string memory p, string memory e, string memory ip, string memory id) public {
 
         nodes[e].publickey = p;
         nodes[e].nodeName = n;
@@ -56,7 +56,7 @@ contract NetworkManagerContract {
         emit print(n, r, p, e, ip, id);
     }
 
-    function getNodeList(uint16 i)  public  constant   returns (string n, string r,string p,string ip,string e, string id) {
+    function getNodeList(uint16 i)  public  view   returns (string memory n, string memory r,string memory p,string memory ip,string memory e, string memory id) {
         
         NodeDetails memory nodeInfo = nodes[enodeList[i]];
         return (
